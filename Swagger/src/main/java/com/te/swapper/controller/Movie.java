d27a0264c4417f9a07ef3aeb5b4017227c3ccf63
AnandVikraman <121006187+AnandVikraman@users.noreply.github.com>
2023-03-07 12:01:16 +0530

    Swagger

---
+++ Swagger/src/main/java/com/te/swapper/controller/Movie.java
@@ -0,0 +1,15 @@
+package com.te.swapper.controller;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Movie {
+	
+	private String actor;
+	private String movieName;
+
+}
---
+++ Swagger/src/test/java/com/te/swapper/SwapperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.te.swapper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwapperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
